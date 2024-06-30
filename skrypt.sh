2f7dc6d3d60b8e4ecddf19296f1a7a447dd46ac0
Jakub Maczka <dsw52737@student.dsw.edu.pl>
2024-06-30 15:23:29 +0200

    Add logs functionality to create 100 log files

--- skrypt.sh
+++ skrypt.sh
@@ -2,3 +2,12 @@
 if [[ $1 == "--date" ]]; then
     date
 fi
+
+if [[ $1 == "--logs" && -z $2 ]]; then
+    for i in $(seq 1 100); do
+        filename="log${i}.txt"
+        echo "Nazwa pliku: $filename" > $filename
+        echo "Nazwa skryptu: skrypt.sh" >> $filename
+        echo "Data utworzenia: $(date)" >> $filename
+    done
+fi 
