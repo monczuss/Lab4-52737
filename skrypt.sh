859af90f3e92e327258e54683a63c9da7b6a5ee3
Jakub Maczka <dsw52737@student.dsw.edu.pl>
2024-06-30 15:36:53 +0200

    Add single dash flags

--- skrypt.sh
+++ skrypt.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-if [[ $1 == "--date" ]]; then
+if [[ $1 == "--date"  || $1 == "-d" ]]; then
     date
 fi
 
-if [[ $1 == "--logs" && -z $2 ]]; then
+if [[ ($1 == "--logs" || $1 == "-l") && -z $2 ]]; then
     for i in $(seq 1 100); do
         filename="log${i}.txt"
         echo "Nazwa pliku: $filename" > $filename
         echo "Nazwa skryptu: skrypt.sh" >> $filename
         echo "Data utworzenia: $(date)" >> $filename
     done
-elif [[ $1 == "--logs" && $2 =~ ^[0-9]+$ ]]; then
+elif [[ ($1 == "--logs" || $1 == "-l") && $2 =~ ^[0-9]+$ ]]; then
     for i in $(seq 1 $2); do
         filename="log${i}.txt"
         echo "Nazwa pliku: $filename" > $filename
@@ -19,7 +19,7 @@ elif [[ $1 == "--logs" && $2 =~ ^[0-9]+$ ]]; then
     done
 fi
 
-if [[ $1 == "--help" ]]; then
+if [[ $1 == "--help" || $1 == "-h" ]]; then
     echo "Dostępne opcje:"
     echo "--date        Wyświetla dzisiejszą datę"
     echo "--logs        Tworzy 100 plików logx.txt"
